ea1263c93dc329c5e28186d0f5abc845f22549fe
rakkaCircl <rakka.purnama@circl.it>
2018-11-12 21:49:52 +0700

    123

--- app/src/main/java/com/sleekr/kata/tebak/tebakkatasleekr/MainActivity.java
+++ app/src/main/java/com/sleekr/kata/tebak/tebakkatasleekr/MainActivity.java
@@ -13,6 +13,9 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-//tes
 
+    private void cek(){
+
+
+    }
 }
